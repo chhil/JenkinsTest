7fa3516621431856ac893db287427fed9af80a23
Murtuza Chhil <chhil@users.noreply.github.com>
2017-11-19 12:10:50 +0530

    Initial

---
+++ src/test/java/org/boring/MainTest.java
@@ -0,0 +1,19 @@
+package org.boring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+
+
+    @Test
+    public void test() throws Exception {
+
+        String a = "1";
+        String b = "1";
+        assertEquals(a, b);
+
+    }
+
+}
